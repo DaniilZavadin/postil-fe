b0cb3e63a6f344c640713b6cf0ccdc47e20767f9
Tetiana Podleska <podleskatetiana@gmail.com>
2021-05-17 16:26:39 +0300

    add item fields

--- src/components/ItemPage.jsx
+++ src/components/ItemPage.jsx
@@ -5,8 +5,15 @@ const ItemPage = () => {
     return (
         <div className='item'>
             <div className='item__img-wrapper'>
-                <img src={item.url} alt='sheets' className='item__img' />
-                <div className='item__info'></div>
+                <img src={item.imageUrls[0]} alt='bedsheets' className='item__img' />
+                <div className='item__info'>
+                    <p className='item__name'>{item.name}</p>
+                    <p className='item__id'>product id: {item._id}</p>
+                    <p className='item__color'>{item.color}</p>
+                    <p classname='item__size'>size {item.sizes}</p>
+                    <p className='item__price'>USD $ {item.currentPrice}</p>
+                    
+                </div>
             </div>
         </div>
     );
